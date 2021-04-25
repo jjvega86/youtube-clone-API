5d80871b50a37254b916db0f0fea9c30e90b7f0f
JJ <jj.vega86@gmail.com>
2021-04-25 15:07:55 -0400

    Installed CORS to API

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const connectDb = require("./startup/db");
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const videos = require('./routes/videos')
+const videos = require("./routes/videos");
 
 connectDb();
 
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
-app.use('/api/videos', videos)
+app.use("/api/videos", videos);
 const port = process.env.port || 5000;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
--- models/video.js
+++ models/video.js
@@ -1,30 +1,45 @@
-const mongoose = require('mongoose')
-const Joi = require('joi')
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const videoSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 2, maxlength: 255},
-    videoId: {type: Number, required: true},
-    description: {type: String, required: true, minlength: 2, maxlength: 1000},
-    comments: [{
+  name: { type: String, required: true, minlength: 2, maxlength: 255 },
+  videoId: { type: Number, required: true },
+  description: { type: String, required: true, minlength: 2, maxlength: 1000 },
+  comments: [
+    {
+      author: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50,
+      },
+      date: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20,
+      },
+      text: {
         type: String,
         required: true,
         minlength: 2,
         maxlength: 5000,
-        default: {}
-    }],
-})
+      },
+    },
+  ],
+});
 
-const Video = mongoose.model('Video', videoSchema)
+const Video = mongoose.model("Video", videoSchema);
 
-function validateVideo(video){
-    const schema = Joi.object({
-        name: Joi.string().min(2).max(255).required(),
-        videoId: Joi.number().required(),
-        description: Joi.string().min(2).max(1000).required()
-    });
+function validateVideo(video) {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(255).required(),
+    videoId: Joi.number().required(),
+    description: Joi.string().min(2).max(1000).required(),
+  });
 
-    return schema.validate(video);
+  return schema.validate(video);
 }
-exports.Video = Video
+exports.Video = Video;
 exports.validate = validateVideo;
 exports.videoSchema = videoSchema;
